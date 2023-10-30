cb4bfe18674be56374dc9f255ac7e608012e5040
JulianNieva <juliannieva90@gmail.com>
2023-10-30 15:25:41 -0300

    Agregado favicon y environment firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    apiKey: "AIzaSyBeQCqlXZ7Ocrjwrux1Y5IyWqxPHaUHpr4",
+    authDomain: "clinicaonline-f8262.firebaseapp.com",
+    projectId: "clinicaonline-f8262",
+    storageBucket: "clinicaonline-f8262.appspot.com",
+    messagingSenderId: "555095323499",
+    appId: "1:555095323499:web:6911001a4b1b0f4264522e"
+};
